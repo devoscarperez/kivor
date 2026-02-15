b02afbd896ac1164f69eca28b08e4916d5edc630
devoscarperez <144189730+devoscarperez@users.noreply.github.com>
2026-02-15 17:50:11 -0400

    Update main.py con Token

--- main.py
+++ main.py
@@ -157,3 +157,11 @@ def login_password(data: dict = Body(...)):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
+def create_access_token(data: dict, expires_delta: timedelta = None):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + (expires_delta or timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+

2a5c70f1e8b0102e86239cfd864ace12e4013a74
devoscarperez <144189730+devoscarperez@users.noreply.github.com>
2026-02-15 12:51:21 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 from fastapi import FastAPI
+import os
+import psycopg2
 
 app = FastAPI(title="KIVOR Backend")
+
+
+def get_connection():
+    database_url = os.getenv("DATABASE_URL")
+    if not database_url:
+        raise Exception("DATABASE_URL no está configurada")
+    return psycopg2.connect(database_url)
+
 
 @app.get("/")
 def root():
     return {"status": "ok", "service": "kivor-backend"}
 
+
 @app.get("/health")
 def health():
     return {"healthy": True}
+

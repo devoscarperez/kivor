f2ec66a9d91522b12533428709db54378150a957
Oscar Perez <144189730+devoscarperez@users.noreply.github.com>
2026-03-15 20:32:49 -0300

    Update db.py

--- core /db.py
+++ core /db.py
@@ -1,1 +1,31 @@
+import psycopg
+import os
+from fastapi import Depends
+from .security import verify_token
 
+def get_connection():
+
+    database_url = os.getenv("DATABASE_URL")
+
+    if not database_url:
+        raise Exception("DATABASE_URL no configurada")
+
+    return psycopg.connect(database_url)
+
+
+def set_tenant_schema(conn, tenant_schema):
+
+    with conn.cursor() as cur:
+        cur.execute(f"SET search_path TO {tenant_schema}, public")
+
+
+def get_tenant_connection(current_user = Depends(verify_token)):
+
+    conn = get_connection()
+
+    set_tenant_schema(conn, current_user["tenant_schema"])
+
+    try:
+        yield conn
+    finally:
+        conn.close()

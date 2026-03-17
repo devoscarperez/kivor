f2b03fd5c1a76f73efa36359b85b93e33f02e060
Oscar Perez <144189730+devoscarperez@users.noreply.github.com>
2026-03-16 22:45:06 -0300

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from typing import Optional
 import psycopg
 import hashlib
 import os
+from urllib.parse import urlparse, parse_qs, urlencode, urlunparse
 
 # =========================
 # MODELOS
@@ -42,16 +43,31 @@ security = HTTPBearer()
 # =========================
 
 def get_connection():
+
     database_url = os.getenv("DATABASE_URL")
 
     if not database_url:
         raise Exception("DATABASE_URL no está configurada")
 
-    # psycopg3 no soporta algunos parámetros como options
-    if "options=" in database_url:
-        database_url = database_url.split("&options=")[0]
+    parsed = urlparse(database_url)
 
-    return psycopg.connect(database_url)
+    query = parse_qs(parsed.query)
+
+    # eliminar parámetro problemático
+    query.pop("options", None)
+
+    new_query = urlencode(query, doseq=True)
+
+    clean_url = urlunparse((
+        parsed.scheme,
+        parsed.netloc,
+        parsed.path,
+        parsed.params,
+        new_query,
+        parsed.fragment
+    ))
+
+    return psycopg.connect(clean_url)
 
 
 # =========================

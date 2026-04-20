98096b404ac2dd0be5a8f665cdc1b93dd0ef7a99
Oscar Perez <144189730+devoscarperez@users.noreply.github.com>
2026-04-19 23:08:49 -0400

    ganancias

--- services/analytics_service.py
+++ services/analytics_service.py
@@ -1,5 +1,39 @@
 from core.db import get_connection
 
+def get_ganancias_por_mes_service(mes: str, tenant_schema: str):
+
+    from core.db import get_connection, set_tenant_schema
+
+    query = """
+    SELECT 
+        TO_CHAR(v.date,'YYYYMM') AS fecha,
+        FLOOR(SUM(CASE WHEN v.family='CABELLO' THEN (v.listprice-v.amounttopayprofessional-v.salondiscount) ELSE 0 END)/(1+(19.0/100))) AS cabello,
+        FLOOR(SUM(CASE WHEN v.family='MANOS_Y_PIES' THEN (v.listprice-v.amounttopayprofessional-v.salondiscount) ELSE 0 END)/(1+(19.0/100))) AS manos_y_pies,
+        FLOOR(SUM(CASE WHEN v.family='DEPILACION' THEN (v.listprice-v.amounttopayprofessional-v.salondiscount) ELSE 0 END)/(1+(19.0/100))) AS depilacion,
+        FLOOR(SUM(CASE WHEN v.family='CEJAS_Y_PESTAÑAS' THEN (v.listprice-v.amounttopayprofessional-v.salondiscount) ELSE 0 END)/(1+(19.0/100))) AS cejas_y_pestanas,
+        FLOOR(SUM(CASE WHEN v.family='FACIALES' THEN (v.listprice-v.amounttopayprofessional-v.salondiscount) ELSE 0 END)/(1+(19.0/100))) AS faciales,
+        FLOOR(SUM(CASE WHEN v.family='CORPORAL' THEN (v.listprice-v.amounttopayprofessional-v.salondiscount) ELSE 0 END)/(1+(19.0/100))) AS corporal
+    FROM sales v
+    WHERE TO_CHAR(v.date,'MM') = %s
+    AND v.family IN ('CABELLO','MANOS_Y_PIES','DEPILACION','CEJAS_Y_PESTAÑAS','FACIALES','CORPORAL')
+    GROUP BY TO_CHAR(v.date,'YYYYMM')
+    ORDER BY fecha;
+    """
+
+    with get_connection() as conn:
+        set_tenant_schema(conn, tenant_schema)
+        with conn.cursor() as cur:
+            cur.execute(query, (mes,))
+            columns = [desc[0] for desc in cur.description]
+            rows = cur.fetchall()
+
+    result = [dict(zip(columns, row)) for row in rows]
+
+    return {
+        "mes": mes,
+        "data": result
+    }
+
 def get_precios_service(
     family: str,
     level2: str = None,

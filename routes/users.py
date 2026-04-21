4dd0fccaa653cef27ff56562efcac1785ff4b5ac
Oscar Perez <144189730+devoscarperez@users.noreply.github.com>
2026-04-21 14:12:46 -0400

    Update users.py

--- routes/users.py
+++ routes/users.py
@@ -1,22 +1,16 @@
 from fastapi import APIRouter, Body
 from core.db import get_connection
+from schemas.user_schema import CreateUserRequest
 
 router = APIRouter()
 
 
 @router.post("/users")
-def create_user(data: dict = Body(...)):
-
-    print("==== CREATE USER START ====")
-    print("DATA:", data)
+def create_user(data: CreateUserRequest):
 
     try:
         conn = get_connection()
-        print("DB CONNECTED")
-
         cur = conn.cursor()
-
-        print("EXECUTING INSERT...")
 
         cur.execute("""
             INSERT INTO core."user" (
@@ -30,29 +24,21 @@ def create_user(data: dict = Body(...)):
             VALUES (%s, %s, %s, %s, %s, %s)
             RETURNING user_id
         """, (
-            data["user_nickname"],
-            data["user_name"],
-            data["user_password"],
-            data["user_firstname"],
-            data["user_lastname"],
-            int(data["user_group_id"])
+            data.user_nickname,
+            data.user_name,
+            data.user_password,
+            data.user_firstname,
+            data.user_lastname,
+            int(data.user_group_id)
         ))
 
-        print("INSERT OK")
-
         user_id = cur.fetchone()
-        print("USER ID:", user_id)
 
         conn.commit()
-        print("COMMIT OK")
-
         cur.close()
         conn.close()
-
-        print("==== CREATE USER END ====")
 
         return {"user_id": user_id}
 
     except Exception as e:
-        print("ERROR CREATE USER:", str(e))
         return {"detail": str(e)}

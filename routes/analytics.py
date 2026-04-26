fa3207738ba8f4629b5674c128a4888344dd04a3
Oscar Perez <144189730+devoscarperez@users.noreply.github.com>
2026-04-25 22:32:36 -0400

    Prueba con GSheet

--- routes/analytics.py
+++ routes/analytics.py
@@ -66,11 +66,5 @@ def obtener_precios(
     return get_precios_service(family, level2, level3, level4)
 
 @router.get("/preciosGS")
-def obtener_precios(
-    family: str,
-    level2: str = None,
-    level3: str = None,
-    level4: str = None
-):
 
     return get_precios_service_GS()

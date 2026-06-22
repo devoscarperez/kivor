c72d1aba67c6412962d4b00751bcb38de2de748f
Oscar Perez <144189730+devoscarperez@users.noreply.github.com>
2026-06-22 00:12:44 -0400

    Update main.py

--- main.py
+++ main.py
@@ -44,6 +44,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+app.include_router(ventas_lyl.router)
 app.include_router(auth.router)
 app.include_router(customers_express.router)
 app.include_router(users.router)

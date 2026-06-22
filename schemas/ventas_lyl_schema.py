47145806da7f465258a880dc4dd2d3824e0b25b3
Oscar Perez <144189730+devoscarperez@users.noreply.github.com>
2026-06-22 00:07:41 -0400

    Update ventas_lyl_schema.py

--- schemas/ventas_lyl_schema.py
+++ schemas/ventas_lyl_schema.py
@@ -1,6 +1,11 @@
 from pydantic import BaseModel
 
+
 class UploadVentasResponse(BaseModel):
     success: bool
+    anio: int
+    mes: int
+    anio_mes: str
     rows_deleted: int
     rows_inserted: int
+    message: str

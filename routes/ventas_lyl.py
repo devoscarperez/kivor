0b0d98e362c654e5e2569685bde6d623c280f027
Oscar Perez <144189730+devoscarperez@users.noreply.github.com>
2026-06-22 00:08:39 -0400

    Update ventas_lyl.py

--- routes/ventas_lyl.py
+++ routes/ventas_lyl.py
@@ -1,8 +1,19 @@
-router = APIRouter(prefix="/ventas-lyl")
+from fastapi import APIRouter, UploadFile, File, Form, Depends, HTTPException
+from core.security import verify_token
+from services.ventas_lyl_service import upload_ventas_service
+from schemas.ventas_lyl_schema import UploadVentasResponse
 
-@router.post("/upload")
+router = APIRouter(prefix="/ventas-lyl", tags=["Ventas LYL"])
+
+
+@router.post("/upload", response_model=UploadVentasResponse)
 async def upload_ventas(
-        anio: int,
-        mes: int,
-        file: UploadFile = File(...)
+    anio: int = Form(...),
+    mes: int = Form(...),
+    file: UploadFile = File(...),
+    current_user: dict = Depends(verify_token)
 ):
+    try:
+        return await upload_ventas_service(anio, mes, file, current_user)
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
